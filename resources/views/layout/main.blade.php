1e22b654cbb3d1ce235f617543de8d9fac6e5ab0
allisali <aliakbaradalahsaya@gmail.com>
2022-10-04 08:49:17 +0700

    Update View and Layout
    Separate content with yield and extends

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home | E-Shopper</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/prettyPhoto.css" rel="stylesheet">
+    <link href="assets/css/price-range.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <script src="assets/js/html5shiv.js"></script>
+    {{-- <script src="assets/js/respond.min.js"></script> --}}
+    {{-- <link rel="shortcut icon" href="assets/images/ico/favicon.ico"> --}}
+
+    {{--    <link rel="apple-touch-icon-precomposed" href="assets/images/ico/apple-touch-icon-57-precomposed.png"> --}}
+</head>
+
+<body>
+    <header id="header">
+        <!--header-->
+        @include('header.header-top')
+        <!--/header_top-->
+        @include('header.header-midle')
+        <!--/header-middle-->
+        @include('header.header-bottom')
+        <!--/header-bottom-->
+    </header>
+    <!--/header-->
+
+
+    <section>
+        <div class="container">
+            @yield('index')
+        </div>
+    </section>
+
+    <footer id="footer">
+        <!--Footer-->
+        @include('footer.footer-top')
+        @include('footer.footer-midle')
+        @include('footer.footer-bot')
+
+
+
+
+
+    </footer>
+    <!--/Footer-->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.scrollUp.min.js"></script>
+    <script src="assets/js/price-range.js"></script>
+    <script src="assets/js/jquery.prettyPhoto.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
